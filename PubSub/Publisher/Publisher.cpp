a744f69061b8bfeb7f37fd0a930e5af3619e1be1
nemanjaa21 <nemanja_malinovic@yahoo.com>
2022-12-30 19:45:09 +0100

    Publisher logika
    Dodat i kompletiran header file, uspostavljena komunikacija sa serverom

--- PubSub/Publisher/Publisher.cpp
+++ PubSub/Publisher/Publisher.cpp
@@ -1,20 +1,184 @@
-// Publisher.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "Publisher.h"
 
-#include <iostream>
+HANDLE publisherSendThread, publisherReceiveThread;
+DWORD publisherSendID, publisherReceiveID;
 
-int main()
-{
-	std::cout << "Hello World!\n";
+/*Funkcija se izvrsava i kreira u thread-u na pocetku main-a,slanje poruka ka serveru */
+DWORD WINAPI PublisherSend(LPVOID lpParam) {
+	SOCKET connectSocket = *(SOCKET*)lpParam;
+
+	while (appRunning && !serverStopped) {
+
+		PrintMenu();
+		char input = _getch();
+
+		char* message = (char*)malloc(270 * sizeof(char));
+
+		if (input == '1' || input == '2' || input == '3' || input == '4' || input == '5') {
+
+			ProcessInput(input, message);
+
+			char* publishMessage = (char*)malloc(250 * sizeof(char));
+
+			EnterAndGenerateMessage(publishMessage, message);
+
+			int messageDataSize = strlen(message) + 1;
+			int messageSize = messageDataSize + sizeof(int);
+
+			MessageStruct* messageStructToSend = GenerateMessageStruct(message, messageDataSize);
+			int sendResult = SendFunction(connectSocket, (char*)messageStructToSend, messageSize);
+			free(messageStructToSend);
+			free(message);
+			free(publishMessage);
+			if (sendResult == -1)
+				break;
+
+
+		}
+		else if (input == 'x' || input == 'X') {
+			strcpy(message, "p:shutDown");
+			int messageDataSize = strlen(message) + 1;
+			int messageSize = messageDataSize + sizeof(int);
+
+			MessageStruct* messageStruct = GenerateMessageStruct(message, messageDataSize);
+			int sendResult = SendFunction(connectSocket, (char*)messageStruct, messageSize);
+			free(messageStruct);
+			free(message);
+			if (sendResult == -1) {
+				break;
+			}
+
+			appRunning = false;
+			closesocket(connectSocket);
+			break;
+		}
+		else {
+			printf("Invalid input.\n");
+			free(message);
+			continue;
+		}
+
+
+
+
+	}
+	return 1;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+/*Funkciju koristi thread, za slanje*/
+DWORD WINAPI PublisherReceive(LPVOID lpParam) {
+	int iResult = 0;
+	SOCKET connectSocket = *(SOCKET*)lpParam;
+	char recvbuf[DEFAULT_BUFLEN];
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	while (appRunning && !serverStopped)
+	{
+		char* recvRes;
+
+		recvRes = ReceiveFunction(connectSocket, recvbuf);
+
+		if (!strcmp(recvRes, "ErrorS")) {
+			closesocket(connectSocket);
+			appRunning = false;
+			serverStopped = true;
+			free(recvRes);
+			break;
+		}
+		else if (!strcmp(recvRes, "ErrorC"))
+		{
+			printf("\nConnection with server closed.\n");
+			printf("Press any key to close this window . . .");
+			closesocket(connectSocket);
+			appRunning = false;
+			serverStopped = true;
+			free(recvRes);
+			break;
+		}
+		else if (!strcmp(recvRes, "ErrorR"))
+		{
+			printf("recv failed with error: %d\n", WSAGetLastError());
+			closesocket(connectSocket);
+			appRunning = false;
+			serverStopped = true;
+			free(recvRes);
+			break;
+		}
+		free(recvRes);
+
+	}
+
+	return 1;
+}
+
+int __cdecl main(int argc, char **argv)
+{
+	SOCKET connectSocket = INVALID_SOCKET;
+	int iResult;
+
+	if (InitializeWindowsSockets() == false)
+	{
+		return 1;
+	}
+
+	connectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (connectSocket == INVALID_SOCKET)
+	{
+		printf("socket failed with error: %ld\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+	sockaddr_in serverAddress;
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serverAddress.sin_port = htons(DEFAULT_PORT);
+
+	if (connect(connectSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
+	{
+		printf("Unable to connect to server.\n");
+		closesocket(connectSocket);
+		WSACleanup();
+	}
+
+	unsigned long int nonBlockingMode = 1;
+	iResult = ioctlsocket(connectSocket, FIONBIO, &nonBlockingMode);
+
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("ioctlsocket failed with error: %ld\n", WSAGetLastError());
+		return 1;
+	}
+
+
+	int connectResult = Connect(connectSocket);
+	if (connectResult == -1) {
+		serverStopped = true;
+		appRunning = false;
+	}
+
+
+
+	publisherSendThread = CreateThread(NULL, 0, &PublisherSend, &connectSocket, 0, &publisherSendID);
+	publisherReceiveThread = CreateThread(NULL, 0, &PublisherReceive, &connectSocket, 0, &publisherReceiveID);
+
+	while (appRunning || !serverStopped) {
+
+	}
+
+
+	if (publisherSendThread)
+		WaitForSingleObject(publisherSendThread, INFINITE);
+	if (publisherReceiveThread)
+		WaitForSingleObject(publisherReceiveThread, INFINITE);
+
+	SAFE_DELETE_HANDLE(publisherSendThread);
+	SAFE_DELETE_HANDLE(publisherReceiveThread);
+
+	closesocket(connectSocket);
+
+	WSACleanup();
+
+	return 0;
+}
+
